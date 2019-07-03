271907bc33af1bd8ca31328e180656ccdfd33ea3
mcalsada <mcalsada@atlas-sys.com>
2019-07-03 08:31:45 -0400

    Initial

---
+++ Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace WindowsFormsApp1
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.SuspendLayout();
+			// 
+			// webBrowser1
+			// 
+			this.webBrowser1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.webBrowser1.Location = new System.Drawing.Point(12, 57);
+			this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+			this.webBrowser1.Name = "webBrowser1";
+			this.webBrowser1.Size = new System.Drawing.Size(764, 354);
+			this.webBrowser1.TabIndex = 0;
+			this.webBrowser1.Url = new System.Uri("http://microsoft.com", System.UriKind.Absolute);
+			this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.WebBrowser1_DocumentCompleted);
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(676, 12);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(100, 22);
+			this.textBox1.TabIndex = 1;
+			// 
+			// timer1
+			// 
+			this.timer1.Interval = 500;
+				// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.webBrowser1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.WebBrowser webBrowser1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Timer timer1;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+			webBrowser1.Navigate("http://google.com/");
+			webBrowser1.StatusTextChanged += WebBrowser1_StatusTextChanged;
+		}
+
+		private void WebBrowser1_StatusTextChanged(object sender, EventArgs e)
+		{
+			textBox1.Text = webBrowser1.ReadyState.ToString();
+		}
+
+		private void WebBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+		{			
+			var bodyElement = webBrowser1.Document.GetElementsByTagName("body");
+			var methods = bodyElement.GetType().GetMethods();
+			var found = false;
+			foreach (var item in methods)
+			{
+				Console.WriteLine(item.Name);
+				if (item.Name == "get_Item")
+				{
+					MessageBox.Show("Found GetItem");
+					found = true;
+
+					try
+					{
+						object[] args = new object[1];
+						args[0] = "0";
+
+						var result = item.Invoke(bodyElement, args);						
+						MessageBox.Show("Found it..." + (result as HtmlElement).InnerHtml.ToString());
+					}
+					catch (Exception ex)
+					{
+						MessageBox.Show("Error..." + ex.Message + Environment.NewLine + ex.StackTrace);
+					}
+					
+					
+					break;
+				}
+			}
+
+			if (!found)
+			{
+				MessageBox.Show("Missing");
+			}
+
+			
+		}
+	}
+}
